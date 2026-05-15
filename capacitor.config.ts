10fa00de657798a7a04ff5bd91610836e0592564
vanadiuminternational <internationalvanadium@gmail.com>
2026-05-15 23:12:03 +0100

    Allow Android local backend mock access

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,9 @@ const config: CapacitorConfig = {
   appId: 'com.vanadiumdigital.hireready',
   appName: 'HireReady',
   webDir: 'dist',
+  android: {
+    allowMixedContent: true,
+  },
 };
 
 export default config;
